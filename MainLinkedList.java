dd7944edc11fcdc34756695b37dca8c879993673
Udit Sharma <sharmaudit124@gmail.com>
2022-07-31 22:33:27 +0530

    Update MainLinkedList.java

--- MainLinkedList.java
+++ MainLinkedList.java
@@ -1,7 +1,11 @@
 package myLinkedList;
 
 import java.util.Iterator;
-
+/*
+* This is a Main class.
+* Where you can run/test your Linked List using it's main Method.
+* Make sure you have all the classes within same package
+*/
 public class MainLinkedList {
 
     public static void main(String[] args) {
@@ -12,33 +16,27 @@ public class MainLinkedList {
         list.Insert(900);
         list.InsertAt(4, 800);
         list.Traverse();
-        //list.Delete();
-        // list.Traverse();
         list.DeleteAt(0);
-        //list.Traverse();
-        //list.DeleteAt(list.Size());
         list.Traverse();
-        //list.Reverse();
         list.Traverse();
+		
         System.out.println(list.Center());
-        //list.Sort();
+		
         list.Traverse();
         System.out.println(list.contains(300));
         System.out.println(list.Size());
         list.Reverse();
-//        list.Traverse();
-//        for (Integer s : list)
-//            System.out.print(s +" ");
-//        System.out.println();
+       list.Traverse();
+		
+       for (Integer s : list)
+           System.out.print(s +" ");
+       System.out.println();
 
         LinkedListIterator itr = list.iterator();
-
         while (itr.hasNext()) {
             Object element = itr.next();
             System.out.print(element + " ");
         }
         System.out.println();
-
-
-    }
+ 	}		
 }
